4c6a4013b3fe72040c39aebbbab8c092ae53c736
“ajinkyai2015” <ajinkyaindulkar.005@gmail.com>
2024-10-18 17:53:07 -0500

    adding natgate

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -19,8 +19,8 @@ resource "aws_internet_gateway" "igw" {
 # Public Subnet for Bastion Host access
 resource "aws_subnet" "public_subnet" {
   vpc_id                  = aws_vpc.main.id
-  cidr_block              = "10.0.1.0/24"  # Define CIDR block for public subnet
-  availability_zone       = "us-east-1a"   # Adjust according to your region
+  cidr_block              = "10.0.1.0/24"  # Public subnet CIDR block
+  availability_zone       = "us-east-1a"
   map_public_ip_on_launch = true
 
   tags = {
@@ -52,12 +52,11 @@ resource "aws_route_table_association" "public_subnet_association" {
 
 # Bastion Host in the public subnet
 resource "aws_instance" "bastion_host" {
-  ami           = "ami-005fc0f236362e99f" # Replace with an appropriate AMI for your region
-  instance_type = "t2.micro"  # Use the instance type you need
+  ami           = "ami-005fc0f236362e99f"
+  instance_type = "t2.micro"
   subnet_id     = aws_subnet.public_subnet.id
-  key_name           = "cicd-pipeline"
+  key_name      = "cicd-pipeline"
 
-  # Security group for Bastion host to allow SSH
   vpc_security_group_ids = [aws_security_group.bastion_sg.id]
 
   tags = {
@@ -73,7 +72,7 @@ resource "aws_security_group" "bastion_sg" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]  # Allow SSH access from the internet
+    cidr_blocks = ["0.0.0.0/0"]  # Allow SSH access from anywhere
   }
 
   egress {
@@ -100,14 +99,12 @@ output "bastion_host_public_ip" {
   value       = aws_instance.bastion_host.public_ip
 }
 
-######### Here Private Subnet Part Starts #########
-
 # Private Subnet 1
 resource "aws_subnet" "private_subnet_1" {
   vpc_id                  = aws_vpc.main.id
-  cidr_block              = "10.0.2.0/24"  # Define CIDR block for private subnet 1
-  availability_zone       = "us-east-1a"   # Adjust according to your region
-  map_public_ip_on_launch = false  # No public IPs for private subnet
+  cidr_block              = "10.0.2.0/24"
+  availability_zone       = "us-east-1a"
+  map_public_ip_on_launch = false
 
   tags = {
     Name = "cicd-project-private-subnet-1"
@@ -117,16 +114,16 @@ resource "aws_subnet" "private_subnet_1" {
 # Private Subnet 2
 resource "aws_subnet" "private_subnet_2" {
   vpc_id                  = aws_vpc.main.id
-  cidr_block              = "10.0.3.0/24"  # Define CIDR block for private subnet 2
-  availability_zone       = "us-east-1b"   # Adjust according to your region
-  map_public_ip_on_launch = false  # No public IPs for private subnet
+  cidr_block              = "10.0.3.0/24"
+  availability_zone       = "us-east-1b"
+  map_public_ip_on_launch = false
 
   tags = {
     Name = "cicd-project-private-subnet-2"
   }
 }
 
-# Security Group for private instances to allow SSH access only from the Bastion Host
+# Security Group for Private Instances
 resource "aws_security_group" "private_instance_sg" {
   vpc_id = aws_vpc.main.id
 
@@ -134,7 +131,7 @@ resource "aws_security_group" "private_instance_sg" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["${aws_instance.bastion_host.private_ip}/32"]  # Allow SSH from Bastion Host's private IP
+    cidr_blocks = ["${aws_instance.bastion_host.private_ip}/32"]  # Allow SSH from Bastion Host
   }
 
   egress {
@@ -144,6 +141,8 @@ resource "aws_security_group" "private_instance_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  depends_on = [aws_instance.bastion_host]  # Ensure Bastion Host is created first
+
   tags = {
     Name = "cicd-project-private-instance-sg"
   }
@@ -151,7 +150,7 @@ resource "aws_security_group" "private_instance_sg" {
 
 # EC2 Instance in Private Subnet 1
 resource "aws_instance" "private_instance_1" {
-  ami           = "ami-005fc0f236362e99f"  # Directly using the specified AMI
+  ami           = "ami-005fc0f236362e99f"
   instance_type = "t2.micro"
   subnet_id     = aws_subnet.private_subnet_1.id
 
@@ -164,7 +163,7 @@ resource "aws_instance" "private_instance_1" {
 
 # EC2 Instance in Private Subnet 2
 resource "aws_instance" "private_instance_2" {
-  ami           = "ami-005fc0f236362e99f"  # Directly using the specified AMI
+  ami           = "ami-005fc0f236362e99f"
   instance_type = "t2.micro"
   subnet_id     = aws_subnet.private_subnet_2.id
 
@@ -175,19 +174,18 @@ resource "aws_instance" "private_instance_2" {
   }
 }
 
-
 # Allocate an Elastic IP for the NAT Gateway
 resource "aws_eip" "nat_eip" {
-  domain = "vpc"  # Use the updated attribute
+  domain = "vpc"
 }
 
 # NAT Gateway in the Public Subnet
 resource "aws_nat_gateway" "nat" {
   allocation_id = aws_eip.nat_eip.id
-  subnet_id     = aws_subnet.public_subnet.id  # Reference your public subnet
+  subnet_id     = aws_subnet.public_subnet.id
 }
 
-# Private Route Table for the Private Subnet
+# Private Route Table
 resource "aws_route_table" "private" {
   vpc_id = aws_vpc.main.id
 
@@ -196,9 +194,14 @@ resource "aws_route_table" "private" {
   }
 }
 
-# Associate the Private Subnet with the Private Route Table
-resource "aws_route_table_association" "private_subnet_association" {
-  subnet_id      = aws_subnet.private_subnet.id  # Private Subnet ID
+# Associate Private Subnets with the Private Route Table
+resource "aws_route_table_association" "private_subnet_1_association" {
+  subnet_id      = aws_subnet.private_subnet_1.id
+  route_table_id = aws_route_table.private.id
+}
+
+resource "aws_route_table_association" "private_subnet_2_association" {
+  subnet_id      = aws_subnet.private_subnet_2.id
   route_table_id = aws_route_table.private.id
 }
 
