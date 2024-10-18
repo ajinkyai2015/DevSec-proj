73f4cbb0a64cb7040294fd48f08e539df9b09ad9
“ajinkyai2015” <ajinkyaindulkar.005@gmail.com>
2024-10-18 17:38:59 -0500

    Planning NAT Gateway

--- main.tf
+++ main.tf
@@ -20,3 +20,21 @@ module "iam" {
 provider "aws" {
   region = "us-east-1"
 }
+
+# Elastic IP for NAT Gateway
+resource "aws_eip" "nat_eip" {
+  vpc = true
+}
+
+# NAT Gateway in the public subnet
+resource "aws_nat_gateway" "nat" {
+  allocation_id = aws_eip.nat_eip.id
+  subnet_id     = module.vpc.public_subnet_id  # Use public subnet from VPC module
+}
+
+# Route table for private subnet to route traffic through NAT Gateway
+resource "aws_route" "private_route" {
+  route_table_id         = module.vpc.private_route_table_id  # Use private route table from VPC module
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id         = aws_nat_gateway.nat.id
+}
--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -174,3 +174,23 @@ resource "aws_instance" "private_instance_2" {
     Name = "cicd-project-private-instance-2"
   }
 }
+
+
+# Elastic IP for NAT Gateway
+resource "aws_eip" "nat_eip" {
+  vpc = true
+}
+
+# NAT Gateway in the public subnet
+resource "aws_nat_gateway" "nat" {
+  allocation_id = aws_eip.nat_eip.id
+  subnet_id     = aws_subnet.public_subnet.id  # Public Subnet ID from the VPC module
+}
+
+# Route table for private subnet to route traffic through NAT Gateway
+resource "aws_route" "private_route" {
+  route_table_id         = aws_route_table.private.id  # Route Table for Private Subnet
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id         = aws_nat_gateway.nat.id
+}
+
